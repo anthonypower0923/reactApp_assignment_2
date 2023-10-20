dc1cd7ad021139e5ed3a6502d9b61ddd685ed788
anthonypower0923 <20098384@mail.wit.ie>
2023-10-20 16:27:59 +0100

    Added Upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,3 +1,4 @@
+// Comment because of accidental Git commit
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
