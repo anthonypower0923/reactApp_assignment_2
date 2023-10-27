b5ea4a26b208bc50610674ac188ac394f43fa033
anthonypower0923 <20098384@mail.wit.ie>
2023-10-27 16:15:01 +0100

    Change upcoming movies page to use caching.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -77,7 +77,13 @@ export const getMovie = (args) => {
   export const getUpcomingMovies = () => {
     return fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&nclude_adult=false`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
   };
